b90284dee3f7cc09de7ce0edea8ec93401247e6d
MarcoBeraldo <beraldomar@gmail.com>
2022-07-06 15:58:09 +0200

    milestone 3 try

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ const posts = [
         date: "07/06/2022",
         text: "lorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsum",
         postImage: "tree.jpg",
-        likes: "10",
+        likes: 10,
     },
     {
         id: 2,
@@ -51,7 +51,7 @@ const posts = [
         date: "07/07/2022",
         text: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
         postImage: "mountain.jpg",
-        likes: "10",
+        likes: 20,
     },
     {
         id: 3,
@@ -60,45 +60,54 @@ const posts = [
         date: "08/06/2022",
         text: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
         postImage: '',
-        likes: "10",
+        likes: 30,
     },
 ]
 const container = document.getElementById('container');
-let postContent = ''
 
+let postContent = ''
 for (let i = 0; i < posts.length; i++) {
     const post = posts[i]
     postContent += `<div class="post">
     <div class="post__header">
-      <div class="post-meta">
-        <div class="post-meta__icon">
-          <img class="profile-pic" src="img/${post.authorImage}" alt="Phil Mangione" />
-        </div>
-        <div class="post-meta__data">
-          <div class="post-meta__author">${post.authorName}</div>
-          <div class="post-meta__time">${post.date}</div>
-        </div>
-      </div>
+    <div class="post-meta">
+    <div class="post-meta__icon">
+    <img class="profile-pic" src="img/${post.authorImage}" alt="Phil Mangione" />
+    </div>
+    <div class="post-meta__data">
+    <div class="post-meta__author">${post.authorName}</div>
+    <div class="post-meta__time">${post.date}</div>
+    </div>
+    </div>
     </div>
     <div class="post__text">
     ${post.text}
     </div>
     <div class="post__image">
-      <img src="img/${post.postImage}" alt="" />
+    <img src="img/${post.postImage}" alt="" />
     </div>
     <div class="post__footer">
-      <div class="likes js-likes">
-        <div class="likes__cta">
-          <a class="like-button js-like-button" href="#" data-postid="1">
-            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-            <span class="like-button__label">Mi Piace</span>
-          </a>
-        </div>
-        <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone</div>
-      </div>
+    <div class="likes js-likes">
+    <div class="likes__cta">
+    <a class="like-button js-like-button" href="#" data-postid="${post.id}">
+    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+    <span class="like-button__label">Mi Piace</span>
+    </a>
     </div>
-  </div>
-</div>`
-}
+    <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone</div>
+    </div>
+    </div>
+    </div>
+    </div>`
 
-container.innerHTML = postContent;
+    container.innerHTML = postContent;
+
+    const likeButton = document.getElementsByClassName("like-button")[0];
+    likeButton.addEventListener("click", function () {
+        post.likes += 1;
+        likeButton.classList.toggle('bcg-blue')
+    })
+
+
+
+}
